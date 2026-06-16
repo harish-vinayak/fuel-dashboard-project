68fb26aa7ee3049c9c86501cd0e2b350019cae1a
harish-vinayak <harishjyothimani@gmail.com>
2026-06-16 09:12:05 +0530

    Update fuel_dashboard.py

--- fuel_dashboard.py
+++ fuel_dashboard.py
@@ -16,17 +16,17 @@ from sklearn.model_selection import train_test_split
 def load_data():
 
     df1 = pd.read_csv(
-        r"E:\Harish Important\MCA\Fuel project\MY2022 Fuel Consumption Ratings.csv",
+        "MY2022 Fuel Consumption Ratings.csv",
         encoding="latin1"
     )
 
     df2 = pd.read_csv(
-        r"E:\Harish Important\MCA\Fuel project\Fuel Consumption Ratings 2023.csv",
+        "Fuel Consumption Ratings 2023.csv",
         encoding="latin1"
     )
 
     df3 = pd.read_csv(
-        r"E:\Harish Important\MCA\Fuel project\Fuel Consumption Ratings 2024.csv",
+        "Fuel Consumption Ratings 2024.csv",
         encoding="latin1"
     )
 
